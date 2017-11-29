115ba574d5ea5a200d6501df4f667c4d9cbad0b3
acombsr <acombsr@rose-hulman.edu>
2017-11-29 09:47:00 -0500

    I completed Module 3.

--- src/m3_turtles.py
+++ src/m3_turtles.py
@@ -14,7 +14,7 @@ Authors: David Mutchler, Dave Fisher, Valerie Galluzzi, Amanda Stouder,
 """
 ########################################################################
 #
-# TODO: 1.
+# DONE: 1.
 #  (Yes, that means for YOU to DO things per these instructions:)
 #
 # On Line 13 above, replace  PUT_YOUR_OWN_NAME_HERE  with your OWN name.
@@ -29,7 +29,7 @@ import rosegraphics as rg
 
 ########################################################################
 #
-# TODO: 2.
+# DONE: 2.
 #   Allow this file to use the rosegraphics.py file by marking the src
 #   directory as a "Sources Root".  Do that by right clicking on the src folder,
 #   then selector  Mark Directory As --> Sources Root
@@ -71,13 +71,35 @@ matt = rg.SimpleTurtle('turtle')
 matt.pen = rg.Pen('red', 30)
 matt.speed = 10  # Faster
 matt.backward(50)
-matt.left(90)
-matt.forward(50)
+matt.left(80)
+matt.forward(70)
+matt.pen = rg.Pen('red', 20)
+matt.left(80)
+matt.forward(70)
 
-
+stephen = rg.SimpleTurtle('turtle')
+stephen.pen = rg.Pen('green', 10)
+stephen.speed = 50  # Faster
+stephen.forward(90)
+stephen.right(45)
+stephen.forward(80)
+stephen.right(45)
+stephen.forward(70)
+stephen.right(45)
+stephen.forward(60)
+stephen.right(45)
+stephen.forward(50)
+stephen.right(45)
+stephen.forward(40)
+stephen.right(45)
+stephen.forward(30)
+stephen.right(45)
+stephen.forward(20)
+stephen.right(45)
+stephen.forward(10)
 ########################################################################
 #
-# TODO: 3.
+# DONE: 3.
 #   Add a few more line of your own code above to make one of the
 #   existing Turtles move some more and/or have different
 #   characteristics.
@@ -91,7 +113,7 @@ matt.forward(50)
 
 ########################################################################
 #
-# TODO: 4.
+# DONE: 4.
 #   The code above  CONSTRUCTS  two SimpleTurtle objects and gives those objects NAMES:
 #       dave    matt
 #
@@ -112,7 +134,7 @@ matt.forward(50)
 
 ########################################################################
 #
-# TODO: 5.
+# DONE: 5.
 #   Run one more time to be sure that all is still OK.
 #   Ensure that no blue bars on the scrollbar-thing to the right remain.
 #
