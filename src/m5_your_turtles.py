d11ecbd283abf03b2b4765fdab5acf9260f2602b
acombsr <acombsr@rose-hulman.edu>
2017-11-29 13:09:39 -0500

    I completed Module 5.

--- src/m5_your_turtles.py
+++ src/m5_your_turtles.py
@@ -2,10 +2,10 @@
 Your chance to explore Loops and Turtles!
 
 Authors: David Mutchler, Dave Fisher, Valerie Galluzzi, Amanda Stouder,
-         their colleagues and PUT_YOUR_NAME_HERE.
+         their colleagues and Stephen Acomb.
 """
 ########################################################################
-# TODO: 1.
+# DONE: 1.
 # On Line 5 above, replace  PUT_YOUR_NAME_HERE  with your own name.
 ########################################################################
 
@@ -28,3 +28,24 @@ Authors: David Mutchler, Dave Fisher, Valerie Galluzzi, Amanda Stouder,
 #
 #  Don't forget to COMMIT your work by using  VCS ~ Commit and Push.
 ########################################################################
+import rosegraphics as rg
+window = rg.TurtleWindow()
+yertle = rg.SimpleTurtle('turtle')
+yertle.pen = rg.Pen('blue', 3)
+yertle.speed = 1
+yertle.pen_down()
+for k in range(9):
+    yertle.forward(k*10)
+    yertle.right(k*10)
+
+mertle = rg.SimpleTurtle('turtle')
+mertle.pen = rg.Pen('red', 3)
+mertle.speed = 1
+mertle.pen_down()
+for k in range(9):
+    mertle.forward((10-k)*10)
+    mertle.left((10-k)*10)
+
+print("Presenting the low-poly snowman!")
+
+window.close_on_mouse_click()
